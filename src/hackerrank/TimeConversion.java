93090c91b8a530d7ef91f04f52db837309c930e2
Oliver Klesing <klesingo@yahoo.com>
2017-02-20 10:36:13 -0600

    Initial classes

---
+++ src/hackerrank/TimeConversion.java
@@ -0,0 +1,41 @@
+package hackerrank;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+/**
+ Given a time in -hour AM/PM format, convert it to military (-hour) time.
+ 
+ Note: Midnight is  on a -hour clock, and  on a -hour clock. Noon is  on a -hour clock, and  on a -hour clock.
+ 
+ Input Format
+ 
+ A single string containing a time in -hour clock format (i.e.:  or ), where  and .
+ 
+ Output Format
+ 
+ Convert and print the given time in -hour format, where .
+ 
+ Sample Input
+ 
+ 07:05:45PM
+ Sample Output
+ 
+ 19:05:45
+ ------------------------------------------
+ https://www.hackerrank.com/challenges/time-conversion
+ */
+public class TimeConversion {
+    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String inputTime = scanner.next();
+        scanner.close();
+        LocalTime localTime = LocalTime.parse(inputTime, DateTimeFormatter.ofPattern("hh:mm:ssa"));
+        String militaryFormattedTime = localTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        System.out.println(militaryFormattedTime);
+    }
+    
+}
